02be5352d7d3de5a5be955b98b44369e5e38dcce
Christian Effertz <christian.effertz@bayer.com>
2018-11-26 07:52:12 +0100

    setup git ignore

---
+++ src/main/java/com/github/cheffe/slowprocessor/SlowProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.github.cheffe.slowprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowProcessorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SlowProcessorApplication.class, args);
+  }
+}
